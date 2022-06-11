c4b5eb580e3675942b73baf5c08f9b5d6c4c41be
肖笛根 <xiaodigen@yzgong.com>
2022-06-11 08:55:22 +0800

    feat: 类数组转换数组

---
+++ 类数组转换.js
@@ -0,0 +1,18 @@
+/**
+ * 类数组拥有 length 属性 可以使用下标来访问元素 但是不能使用数组的方法 如何把类数组转化为数组?
+ */
+
+const arrayLike = document.querySelectorAll('div')
+
+ // 1.扩展运算符
+const array = [...arrayLike]
+ // 2.Array.from
+ Array.from(arrayLike)
+ // 3.Array.prototype.slice
+ Array.prototype.slice.call(arrayLike)
+ // 4.Array.apply
+ Array.apply(null, arrayLike)
+ // 5.Array.prototype.concat
+ Array.prototype.concat.apply([], arrayLike)
+ 
+ 
